9923e301d2f938b23fb0721e13a7a1fbdf7b4d30
Hironobu Ohara <47152180+ohiro18@users.noreply.github.com>
2021-01-30 14:05:42 +0900

    Update recsSvc.js

--- elastic-web-app/app/scripts/recsSvc.js
+++ elastic-web-app/app/scripts/recsSvc.js
@@ -3,30 +3,28 @@ angular.module('recsApp')
 
 recsSvc.$inject = ['esClient', '$q', '$http', 'graphUtils'];
 
-
 function recsSvc(esClient, $q, $http, graphUtils) {
   return {
     recommend: recommend
   };
 
-  function fetchMovieDetails(movieIds) {
-    var movies = [];
+  function fetchItemDetails(ItemIds) {
+    var Items = [];
     var deferred = $q.defer();
     esClient.mget({
       index: 'ml_tmdb',
-      type: 'movie',
-      body: {ids: movieIds}}).then(function(resp) {
-        var movies = [];
+      type: 'Item',
+      body: {ids: ItemIds}}).then(function(resp) {
+        var Items = [];
         angular.forEach(resp.docs, function(doc) {
-          var movie = doc._source;
-          if (movie) {
-            movies.push(movie);
+          var Item = doc._source;
+          if (Item) {
+            Items.push(Item);
           }
         });
-        deferred.resolve(movies);
+        deferred.resolve(Items);
       });
     return deferred.promise;
-
   }
 
   function constructGuidingQuery(likes, config) {
@@ -37,14 +35,14 @@ function recsSvc(esClient, $q, $http, graphUtils) {
       };
     }
     var likedIds = [];
-    angular.forEach(likes, function(likedMovie) {
-      likedIds.push(likedMovie.mlensId);
+    angular.forEach(likes, function(likedItem) {
+      likedIds.push(likedItem.mlensId);
     });
-    movieIdqStr = likedIds.join(' OR ');
-    var minShouldMatch = config.minMovies ? config.minMovies : 0;
+    ItemIdqStr = likedIds.join(' OR ');
+    var minShouldMatch = config.minItems ? config.minItems : 0;
     queryClause1 = {"query_string": {
-                      "query": movieIdqStr,
-                      "fields": ["liked_movies"],
+                      "query": ItemIdqStr,
+                      "fields": ["liked_Items"],
                       "minimum_should_match": "" + (minShouldMatch) + "%"
                     }};
     return queryClause1;
@@ -59,13 +57,13 @@ function recsSvc(esClient, $q, $http, graphUtils) {
     var overviewTexts = [];
 
 
-    angular.forEach(likes, function(likedMovie) {
-      if (likedMovie.hasOwnProperty('overview')) {
-        overviewTexts.push(likedMovie.overview);
+    angular.forEach(likes, function(likedItem) {
+      if (likedItem.hasOwnProperty('overview')) {
+        overviewTexts.push(likedItem.overview);
       }
-      angular.forEach(likedMovie.genres, function(genre) {
-        if (likedMovie.hasOwnProperty('release_date')) {
-          var releaseYear = likedMovie.release_date.substr(0,4);
+      angular.forEach(likedItem.genres, function(genre) {
+        if (likedItem.hasOwnProperty('release_date')) {
+          var releaseYear = likedItem.release_date.substr(0,4);
           releaseHalfDecade = (5 * ~~(parseInt(releaseYear) / 5)).toString(); // hack to force integer division
           if (!allYears.hasOwnProperty(releaseHalfDecade)) {
             allYears[releaseHalfDecade] = 0;
@@ -100,7 +98,6 @@ function recsSvc(esClient, $q, $http, graphUtils) {
     });
 
     // include overview text
-
     likeGenreqStr = likedGenres.join(' OR ');
     likeYearQStr = likedHalfDecades.join(' OR ');
 
@@ -136,39 +133,21 @@ function recsSvc(esClient, $q, $http, graphUtils) {
     return query;
   }
 
-
   function recommend(user, config) {
-    //POST movielens/_graph/explore
-    //
-    // The elastic graph plugin works by first sampling the top <sample_size>
-    // results of the query, then measuring the statistical significance (see sig
-    // terms agregration) of terms in those results.
-    //
-    // Then it repeats this process using the significant terms to find related significant
-    // terms to those (though this process is a bit less fuzzy to me)
-    //
-    // The first attempt I want to try is pure "collaborative" filtering, we have
-    // an index with movies 'liked' by a user. Here we find users "more like" this user.
-    // (this is collaborative filtering).
-    //
-    // But graph lets us go a step further and see the movies that are "special" ie
-    // statistically interesting in the users more like me. Then we can find what's
-    // special about those movies, and so on.
-
     var qConfig = angular.copy(config);
     qConfig.globalGuidance = false;
     query = constructAdHocQuery(user.likes, qConfig);
 
     likedIds = [];
-    angular.forEach(user.likes, function(likedMovie) {
-      likedIds.push(likedMovie.mlensId);
+    angular.forEach(user.likes, function(likedItem) {
+      likedIds.push(likedItem.mlensId);
     });
 
     var esQuery=  {
           "query": query,
           "vertices": [
               {
-                  "field": "liked_movies",
+                  "field": "liked_Items",
                   "exclude": likedIds
               }
           ],
@@ -177,70 +156,64 @@ function recsSvc(esClient, $q, $http, graphUtils) {
             "sample_size": config.numSimilarUsers // this many relevant results used in sig terms
           },
           "connections": {
-              "query": constructGuidingQuery(user.likes, {minMovies: 0, globalGuidance: config.globalGuidance}), // guide only in the context of these recs, not globally
+              "query": constructGuidingQuery(user.likes, {minItems: 0, globalGuidance: config.globalGuidance}), // guide only in the context of these recs, not globally
               "vertices": [ // how many degrees of kevin bacon
-                  {"field": "liked_movies",
+                  {"field": "liked_Items",
                    "exclude": likedIds}
               ]
           }
-
     };
-    $http.post(esBaseURL + '/movielens/_graph/explore', esQuery)
+    $http.post(esBaseURL + '/Itemlens/_graph/explore', esQuery)
     .success(function(resp) {
-      // movies of depth 0 are statistically significant in the top <sample_size>
+      // Items of depth 0 are statistically significant in the top <sample_size>
       // search results of the query
-      var allMovies = resp.vertices;
-      var usersMovies = graphUtils.parse(resp.vertices, resp.connections);
+      var allItems = resp.vertices;
+      var usersItems = graphUtils.parse(resp.vertices, resp.connections);
 
       // sort by depth so 0 depth first, helps
       // organize the graph
 
-      relatedMovieIds = [];
-      angular.forEach(allMovies, function(movieVertex) {
-        var movieId = movieVertex.term;
-        relatedMovieIds.push(movieId);
+      relatedItemIds = [];
+      angular.forEach(allItems, function(ItemVertex) {
+        var ItemId = ItemVertex.term;
+        relatedItemIds.push(ItemId);
       });
-      fetchMovieDetails(relatedMovieIds)
-      .then(function success(movieDetails) {
+      fetchItemDetails(relatedItemIds)
+      .then(function success(ItemDetails) {
 
-        // the rest of this is just merging the movie details from
-        // the other collection with the graph structure we parsed
-        // out above
-        //
-
-        // hash by movieId
-        movieDetailsLookup = {};
-        angular.forEach(movieDetails, function(movieDetail) {
-          movieDetailsLookup[movieDetail.mlensId] = movieDetail;
+        // hash by ItemId
+        ItemDetailsLookup = {};
+        angular.forEach(ItemDetails, function(ItemDetail) {
+          ItemDetailsLookup[ItemDetail.mlensId] = ItemDetail;
         });
 
-        // connect movies to one-another via graph
-        angular.forEach(usersMovies, function(userMovie) {
-          var currMovie = movieDetailsLookup[userMovie.term];
-          if (currMovie) {
-            currMovie.related = [];
-            angular.forEach(userMovie.outbound, function(connectedMovie) {
-              var connectedMovieDetailed = movieDetailsLookup[connectedMovie.term];
-              currMovie.related.push(connectedMovieDetailed);
+        // connect Items to one-another via graph
+        angular.forEach(usersItems, function(userItem) {
+          var currItem = ItemDetailsLookup[userItem.term];
+          if (currItem) {
+            currItem.related = [];
+            angular.forEach(userItem.outbound, function(connectedItem) {
+              var connectedItemDetailed = ItemDetailsLookup[connectedItem.term];
+              currItem.related.push(connectedItemDetailed);
             });
           }
         });
 
-        // add details to movies are related to this user
-        var thisUsersMovies = [];
-        angular.forEach(usersMovies, function(movieVertex) {
-          thisUsersMovies.push(movieDetailsLookup[movieVertex.term]);
+        // add details to Items are related to this user
+        var thisUsersItems = [];
+        angular.forEach(usersItems, function(ItemVertex) {
+          thisUsersItems.push(ItemDetailsLookup[ItemVertex.term]);
         });
 
         // organize a graph based on these details
-        user.relatedMovies = thisUsersMovies;
+        user.relatedItems = thisUsersItems;
       }, function error(err) {
         console.log(err);
       }
 
       );
 
-      // movies of depth 1 are significant to the movie at depth 0 it's connected to
+      // Items of depth 1 are significant to the Item at depth 0 it's connected to
 
       console.log(resp);
     });
