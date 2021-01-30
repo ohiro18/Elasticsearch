03cc737a4670504144d1e2c81a62b18aef981b7a
Hironobu Ohara <47152180+ohiro18@users.noreply.github.com>
2021-01-30 14:03:25 +0900

    Update likedMovieSearchCtrl.js

--- elastic-web-app/app/scripts/likedMovieSearchCtrl.js
+++ elastic-web-app/app/scripts/likedMovieSearchCtrl.js
@@ -1,10 +1,10 @@
 angular.module('recsApp')
-  .controller('LikedMovieSearchCtrl', function(esClient) {
-    movieSearch = this;
+  .controller('LikedItemSearchCtrl', function(esClient) {
+    ItemSearch = this;
 
-    movieSearch.searchString = "";
-    movieSearch.hits = [];
-    movieSearch.run = function() {
+    ItemSearch.searchString = "";
+    ItemSearch.hits = [];
+    ItemSearch.run = function() {
       // search ES by title, show results
       var esQuery = {
         "query": {
@@ -12,18 +12,18 @@ angular.module('recsApp')
             "should": [
               {"match": {
                 "title": {
-                  "query": movieSearch.searchString
+                  "query": ItemSearch.searchString
                 }
               }},
               {
                 "match_phrase_prefix": {
-                  "title": movieSearch.searchString
+                  "title": ItemSearch.searchString
                 }
               },
               {
                 "match_phrase_prefix": {
                   "title_sentinel": {
-                    "query":  "SENTINEL_BEGIN " + movieSearch.searchString,
+                    "query":  "SENTINEL_BEGIN " + ItemSearch.searchString,
                     "boost": 100
                   }
                 }
@@ -35,12 +35,12 @@ angular.module('recsApp')
 
       esClient.search({
         index: 'ml_tmdb',
-        type: 'movie',
+        type: 'Item',
         body: esQuery
       }).then(function success(resp) {
-        movieSearch.hits = [];
+        ItemSearch.hits = [];
         angular.forEach(resp.hits.hits, function(hit) {
-          movieSearch.hits.push(hit._source);
+          ItemSearch.hits.push(hit._source);
         });
       });
     };
