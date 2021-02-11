b152d09c28c44104b0e105ac93a544a81e723872
Hironobu Ohara <47152180+ohiro18@users.noreply.github.com>
2021-02-11 19:42:39 +0900

    Create exceptions.py

---
+++ elastic-web-app/etl/exceptions.py
@@ -0,0 +1,17 @@
+
+class ElasticsearchDslException(Exception):
+    pass
+
+
+class UnknownDslObject(ElasticsearchDslException):
+    pass
+
+
+class ValidationException(ValueError, ElasticsearchDslException):
+    pass
+
+
+class IllegalOperation(ElasticsearchDslException):
+    pass
+    
+    
