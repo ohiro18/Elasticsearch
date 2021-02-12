9954020deefe8ab7787510520a378d935d89a543
Hironobu Ohara <47152180+ohiro18@users.noreply.github.com>
2021-02-12 21:36:04 +0900

    Create chat-analytics-server.js

---
+++ chat-analytics/chat-analytics-server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const portNo = 3001
+server.listen(portNo, () => {
+  console.log('Runnig..', 'http://localhost:' + portNo)
+})
+
+app.use('/public', express.static('./public'))
+app.get('/', (req, res) => { 
+  res.redirect(302, '/public')
+})
+
+const socketio = require('socket.io')
+const io = socketio.listen(server)
+
+io.on('connection', (socket) => {
+  console.log('Connect user:', socket.client.id)
+
+  socket.on('chat-msg', (msg) => {
+    console.log('message', msg)
+    io.emit('chat-msg', msg)
+  })
+})
