9807b06120a3b0b579be1c66ba3144ac049ef166
Hironobu Ohara <47152180+ohiro18@users.noreply.github.com>
2021-02-14 21:33:17 +0900

    Update init.sh

--- elastic-web-app/deploy/init/init.sh
+++ elastic-web-app/deploy/init/init.sh
@@ -2,11 +2,18 @@
 
 sleep 15
 
-# echo "Running indexMlTmdb.py"
-# python /etl/indexMlTmdb.py http://elasticsearch:9200 /etl/ml_tmdb.json
+echo "Running __init__.py"
+python /etl/__init__.py
+
+until wait-for-it.sh elasticsearch:9200  ";" ; do
+	echo "Waiting on Elasticsearch init..."
+	sleep 5
+done
+
+echo "Running index.py"
+python /etl/index.py
 
 echo "Running ratingsToEs.py"
 python /etl/ratingsToEs.py http://elasticsearch:9200 /etl/ml_tmdb.json /etl/ml-20m/ratings.csv
 
 echo "Done with setup"
-
